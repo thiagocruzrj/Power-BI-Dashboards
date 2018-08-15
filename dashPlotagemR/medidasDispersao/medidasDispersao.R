18519a1dc9d1f672e7409cc1ce29a3dcb990d360
Thiago Cruz <thagocruz@gmail.com>
2018-08-15 07:05:59 -0300

    Add files via upload

---
+++ dashPlotagemR/medidasDispersao/medidasDispersao.R
@@ -0,0 +1,21 @@
+# Medidas de Dispersão
+
+# Definindo a pasta de trabalho
+# Substitua o caminho abaixo pela pasta no seu computador
+setwd("C:\Users\DELL\Documents\powerBIPortifolio\dashPlotagemR\medidasDispersao")
+getwd()
+
+# Carregando o dataset
+vendas <- read.csv("Vendas.csv", fileEncoding = "windows-1252")
+
+# Resumo do dataset
+View(vendas)
+str(vendas)
+summary(vendas$Valor)
+
+# Variância
+var(vendas$Valor)
+
+# Desvio Padrão
+sd(vendas$Valor)
+
